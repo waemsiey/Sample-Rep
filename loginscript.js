5acca4072f841837708383981edafcf78090fc5e
waemsiey <mobpsy322@gmail.com>
2024-06-12 12:40:24 +0800

    first commit

---
+++ loginscript.js
@@ -0,0 +1,47 @@
+document.getElementById("SigninID").addEventListener("submit", function(event) {
+    event.preventDefault(); 
+
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+
+    Swal.showLoading();
+
+    setTimeout(function() {
+        var storedUsername = localStorage.getItem('username');
+        var storedPassword = localStorage.getItem('password');
+
+        if (username === storedUsername && password === storedPassword) {
+            Swal.fire({
+                title: 'Success!',
+                text: 'Welcome Back ' + username + '!',
+                icon: 'info',
+                confirmButtonText: 'OK'
+            });
+        } else {
+            Swal.fire({
+                title: 'Error!',
+                text: 'invalid username or password',
+                icon: 'warning',
+                confirmButtonText: 'OK'});
+
+            document.getElementById("passwordError").textContent = "Invalid username or password";
+        }
+    }, 2000);
+});
+
+document.getElementById("togglePassword").addEventListener("click", function() {
+    var passwordInput = document.getElementById("password");
+    var toggleIcon = document.getElementById("togglePassword");
+
+    if (passwordInput.type === "password") {
+        passwordInput.type = "text";
+        toggleIcon.classList.remove("bx-hide");
+        toggleIcon.classList.add("bx-show");
+        toggleIcon.title = "Hide Password";
+    } else {
+        passwordInput.type = "password";
+        toggleIcon.classList.remove("bx-show");
+        toggleIcon.classList.add("bx-hide");
+        toggleIcon.title = "Show Password";
+    }
+});
---
+++ registrationscript.js
@@ -0,0 +1,56 @@
+document.getElementById("RegistrationID").addEventListener("submit", function(event) {
+    event.preventDefault(); 
+
+    var username = document.getElementById("username").value;
+    var passwordInput = document.getElementById("password");
+    var cpasswordInput = document.getElementById("cpassword");
+    var password = passwordInput.value;
+    var cpassword = cpasswordInput.value;
+
+    if (password !== cpassword) {
+        cpasswordInput.parentNode.classList.add("error");
+        document.getElementById("passwordError").textContent = "Passwords don't match";
+        return;
+    }
+
+    if (password.length < 8 || !/\d/.test(password)) {
+        passwordInput.parentNode.classList.add("error");
+        document.getElementById("passwordError").textContent = "Password must be at least 8 characters long and contain at least one number";
+        return;
+    } else {
+        passwordInput.parentNode.classList.remove("error");
+        document.getElementById("passwordError").textContent = "";
+    }
+    Swal.showLoading(); 
+
+    setTimeout(function() {
+        localStorage.setItem('username', username);
+        localStorage.setItem('password', password);
+
+        Swal.fire({
+            title: 'Success!',
+            text: 'You have successfully registered',
+            icon: 'success',
+            confirmButtonText: 'OK'
+        });
+    }, 2000); 
+});
+
+
+document.getElementById("togglePassword").addEventListener("click", function() {
+    var passwordInput = document.getElementById("password");
+    var toggleIcon = document.getElementById("togglePassword").querySelector("i");
+    
+    if (passwordInput.type === "password") {
+        passwordInput.type = "text";
+        toggleIcon.classList.remove("bx-hide"); 
+        toggleIcon.classList.add("bx-show");
+        toggleIcon.parentElement.title = "Hide Password";
+    } else {
+        passwordInput.type = "password";
+        toggleIcon.className = "bx bx-hide";
+        toggleIcon.classList.remove("bx-show");
+        toggleIcon.classList.add("bx-hide");
+        toggleIcon.parentElement.title = "Show Password";
+    }
+});
